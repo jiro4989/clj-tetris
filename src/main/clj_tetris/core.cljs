eb7c0f8d12f8f903b2f63d13fe572e55a8e9eb55
jiro4989 <jiroron666@gmail.com>
2020-12-15 17:17:37 +0000

    2, 8, 16進数に対応

--- src/main/clj_tetris/core.cljs
+++ src/main/clj_tetris/core.cljs
@@ -6,9 +6,8 @@
 
 (def height (r/atom 170))
 (def weight (r/atom 60))
-(def decimal (r/atom 255))
 
-(defn text-input [value]
+(defn component-text-input [value]
   [:input {:type "number"
            :value @value
            :on-change #(reset! value (-> % .-target .-value js/parseInt))}])
@@ -18,7 +17,7 @@
       (/ h)
       (/ h)))
 
-(defn ui-bmi []
+(defn component-bmi []
   [:section
    [:h1 "BMI計算"]
    [:hr]
@@ -27,35 +26,45 @@
     [:tbody
      [:tr
       [:td "身長"]
-      [:td (text-input height)]
+      [:td (component-text-input height)]
       [:td "cm"]]
      [:tr
       [:td "体重"]
-      [:td (text-input weight)]
+      [:td (component-text-input weight)]
       [:td "kg"]]]]
    [:hr]
    [:div "計算結果"]
    [:div (str (bmi (/ @height 100) @weight))]])
 
-(defn to-bin [n]
-  (.toString n 2))
+(defn component-to-base [n]
+  (let [decimal (r/atom 255)]
+    (fn []
+      [:tr
+       [:td "10進数"]
+       [:td (component-text-input decimal)]
+       [:td n "進数"]
+       [:td (str (.toString @decimal n))]])))
 
-(defn ui-bin []
+(defn component-to-bases []
   [:section
-   [:h1 "10進数を2進数に変換"]
+   [:h1 "10進数を他進数に変換"]
    [:hr]
    [:table
-    [:tbody
+    [:thead
      [:tr
-      [:td "10進数"]
-      [:td (text-input decimal)]
-      [:td "-> 計算結果"]
-      [:td (str (to-bin @decimal))]]]]])
+      [:td]
+      [:td "入力"]
+      [:td "変換先進数"]
+      [:td "出力"]]]
+    [:tbody
+     [component-to-base 2]
+     [component-to-base 8]
+     [component-to-base 16]]]])
 
 (defn ui []
   [:div
-   (ui-bmi)
-   (ui-bin)])
+   (component-bmi)
+   (component-to-bases)])
 
 ;; lifecycle hook ;;
 (defn ^:dev/before-load stop []
