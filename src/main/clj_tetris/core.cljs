d2d877eac23bb37430d5d1da18b22bcbc902c476
jiro4989 <jiroron666@gmail.com>
2020-12-15 16:48:46 +0000

    進数変換コンポーネントを追加

--- src/main/clj_tetris/core.cljs
+++ src/main/clj_tetris/core.cljs
@@ -6,19 +6,20 @@
 
 (def height (r/atom 170))
 (def weight (r/atom 60))
+(def decimal (r/atom 255))
 
 (defn text-input [value]
   [:input {:type "number"
            :value @value
-           :on-change #(reset! value (-> % .-target .-value))}])
+           :on-change #(reset! value (-> % .-target .-value js/parseInt))}])
 
 (defn bmi [h w]
   (-> w
       (/ h)
       (/ h)))
 
-(defn ui []
-  [:div
+(defn ui-bmi []
+  [:section
    [:h1 "BMI計算"]
    [:hr]
    [:span "以下に入力してね"]
@@ -35,6 +36,26 @@
    [:hr]
    [:div "計算結果"]
    [:div (str (bmi (/ @height 100) @weight))]])
+
+(defn to-bin [n]
+  (.toString n 2))
+
+(defn ui-bin []
+  [:section
+   [:h1 "10進数を2進数に変換"]
+   [:hr]
+   [:table
+    [:tbody
+     [:tr
+      [:td "10進数"]
+      [:td (text-input decimal)]
+      [:td "-> 計算結果"]
+      [:td (str (to-bin @decimal))]]]]])
+
+(defn ui []
+  [:div
+   (ui-bmi)
+   (ui-bin)])
 
 ;; lifecycle hook ;;
 (defn ^:dev/before-load stop []
