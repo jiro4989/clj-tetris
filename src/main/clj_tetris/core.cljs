1168e38654a2ec1d3370739406b82b4bd7ace7fa
jiro4989 <jiroron666@gmail.com>
2020-12-15 16:25:49 +0000

    BMIを計算する機能を追加

--- src/main/clj_tetris/core.cljs
+++ src/main/clj_tetris/core.cljs
@@ -4,8 +4,37 @@
 
 (def dom-root (js/document.getElementById "root"))
 
+(def height (r/atom 170))
+(def weight (r/atom 60))
+
+(defn text-input [value]
+  [:input {:type "number"
+           :value @value
+           :on-change #(reset! value (-> % .-target .-value))}])
+
+(defn bmi [h w]
+  (-> w
+      (/ h)
+      (/ h)))
+
 (defn ui []
-  [:div "hello world"])
+  [:div
+   [:h1 "BMI計算"]
+   [:hr]
+   [:span "以下に入力してね"]
+   [:table
+    [:tbody
+     [:tr
+      [:td "身長"]
+      [:td (text-input height)]
+      [:td "cm"]]
+     [:tr
+      [:td "体重"]
+      [:td (text-input weight)]
+      [:td "kg"]]]]
+   [:hr]
+   [:div "計算結果"]
+   [:div (str (bmi (/ @height 100) @weight))]])
 
 ;; lifecycle hook ;;
 (defn ^:dev/before-load stop []
