97cd0056ac93305f4f03b5644bf9ac60a01ff663
jiro4989 <jiroron666@gmail.com>
2020-12-15 15:51:45 +0000

    add lifecycle hook

--- src/main/clj_tetris/core.cljs
+++ src/main/clj_tetris/core.cljs
@@ -1,4 +1,12 @@
 (ns clj-tetris.core)
 
+;; lifecycle hook ;;
+(defn ^:dev/before-load stop []
+  (js/console.log "stop"))
+
+;; lifecycle hook ;;
+(defn ^:dev/after-load start []
+  (js/console.log "start"))
+
 (defn init []
-  (println "Hello World"))
+  (println "test"))
